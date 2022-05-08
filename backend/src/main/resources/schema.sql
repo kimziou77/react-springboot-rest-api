a1a8081bb485f000a24fbb9b0ec0e4503665a816
kimziou77 <kimziou77@naver.com>
2022-05-08 09:07:45 +0900

    1 셋팅 Config 깃이그노어json Setting2

---
+++ backend/src/main/resources/schema.sql
@@ -0,0 +1,44 @@
+CREATE TABLE product
+(
+    id          bigint primary key auto_increment,
+
+    name        VARCHAR(20) not null,
+    category    VARCHAR(50) not null,
+    price       bigint      not null,
+    quantity    bigint      not null,
+    description VARCHAR(500) default null,
+    image_path varchar(200) default null,
+    created_at  datetime(6) not null,
+    updated_at  datetime(6) not null
+);
+
+CREATE TABLE orders
+(
+    id           bigint primary key auto_increment,
+
+    email        VARCHAR(50)  not null,
+    address      VARCHAR(200) not null,
+    total_price  bigint       not null,
+    order_status VARCHAR(50)  not null,
+    created_at   datetime(6)  not null,
+    updated_at   datetime(6)  not null
+);
+
+CREATE TABLE order_product
+(
+    id          bigint primary key auto_increment,
+
+    order_id    bigint      not null,
+    product_id  bigint      not null,
+
+    count       bigint      not null,
+    total_price bigint      not null,
+
+    created_at  datetime(6) not null,
+    updated_at  datetime(6) not null,
+
+    index (order_id),
+
+    constraint fk_order_product_to_orders foreign key (order_id) references orders (id) on delete cascade,
+    constraint fk_order_product_to_product foreign key (product_id) references product (id)
+);
